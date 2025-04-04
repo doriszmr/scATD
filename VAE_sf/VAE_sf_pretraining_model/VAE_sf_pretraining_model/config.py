44d506edd93b303c40c3cd3e88a29f8de28a2666
luozeyu1024 <101546939+luozeyu1024@users.noreply.github.com>
2025-04-04 04:36:11 -0400

    Add files via upload

---
+++ VAE_sf/VAE_sf_pretraining_model/VAE_sf_pretraining_model/config.py
@@ -0,0 +1,10 @@
+
+config = {
+    'input_dim': 3072,
+    'z_dim': 421,
+    'hidden_dim_layer0': 1926,
+    'hidden_dim_layer_out_Z': 262,
+    'num_blocks': 2,
+    'Encoder_layer_dims': [3072,1926,850,262],
+    'Decoder_layer_dims': [421,850,1926]
+}
