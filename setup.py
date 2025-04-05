2bfa6d8adff9d7f0ce0a1bcb98f284d0b9dc85b8
luozeyu1024 <101546939+luozeyu1024@users.noreply.github.com>
2025-04-05 01:59:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ setup(
         'scikit-learn==1.5.1',
         'numpy==1.26.4',
         'pandas==2.2.2',
-        'openpyxl'=='3.1.5',
+        'openpyxl==3.1.5',
+        'optuna==3.6.1',
+        'imbalanced-learn==0.13.0'
     ],
     extras_require={
         'cuda': [
